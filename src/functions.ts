83076ddc40b19232e23071c601f605ba6c4917a3
Jan Šfaffa <agenttupesy@gmail.com>
2021-08-05 23:35:31 +0200

    done some work

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,7 @@
+export const randomInt = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
 export const isPrime = (n: number) => {
   if (n === 1 || n === 3) return true;
 
@@ -39,132 +43,47 @@ export const lcm = (n1: number, n2: number): number => {
   return !n1 || !n2 ? 0 : Math.abs((n1 * n2) / gcd(n1, n2));
 };
 
-export const lcmm = (numbers: number[]) => {
-  let response = 1;
-  for (const n of numbers) {
-    response = lcm(n, response);
-  }
-  return response;
-};
-export const getFactors = (n: number) => {
-  let response: number[] = [];
-  for (let i = 1; i <= n; i++) {
-    if (n % i == 0) {
-      response.push(i);
-    }
-  }
-  return response;
-};
+// export const lcmm = (numbers: number[]) => {
+//   let response = 1;
+//   for (const n of numbers) {
+//     response = lcm(n, response);
+//   }
+//   return response;
+// };
 
-export const getFactorPair = (n: number) => {
-  let response: number[] | undefined = [];
-  for (let i = 2; i < n; i++) {
-    if (n % i === 0) {
-      response.push(i);
-      response.push(n / i);
-      break;
-    }
+// https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm#Python
+export const extendedEuclideanAlgorithm = (n: number, m: number): number[] => {
+  if (n === 0) {
+    return [m, 0, 1];
   }
-  return response.length > 0 ? response : undefined;
-};
-export const getCoprimeFactorPair = (n: number) => {
-  let response: number[] | undefined = [];
-  for (let i = 2; i < n; i++) {
-    if (gcd(i, n / i) === 1) {
-      response.push(i);
-      response.push(n / i);
-      break;
-    }
-  }
-  return response.length > 0 ? response : undefined;
-};
-export const isOdd = (n: number) => !((n + 1) % 2 === 1);
-export const isPowerOfOddPrime = (n: number): boolean => {
-  if (isPrime(n)) return false;
-  for (let i = 2; i < n; i++) {
-    const root = Math.pow(n, 1 / i);
-    if (root % 1 !== 0) continue;
-    console.log('root', root, i, n);
-    if (isOdd(root)) return true;
-  }
+  const m_div_n = Math.floor(m / n),
+    m_mod_n = m % n;
 
-  return false;
-  //   if (!isOdd(n)) return false;
-  //   const subFactorPair = getFactorPair(n);
-
-  //   if (
-  //     subFactorPair &&
-  //     gcd(subFactorPair[0], subFactorPair[1]) === subFactorPair[0] &&
-  //     gcd(subFactorPair[0], subFactorPair[1]) === subFactorPair[1]
-  //   ) {
-  //     return true;
-  //   }
-  //   return false;
+  const [g, x, y] = extendedEuclideanAlgorithm(m_mod_n, n);
+  return [g, y - m_div_n * x, x];
+};
+export const modularInverse = (n: number, m: number) => {
+  const [g, x, _] = extendedEuclideanAlgorithm(n, m);
+  if (g !== 1) return 0;
+  return x % m;
 };
 
-export const eulersTotient = (n: number) => {
-  let totalCoprimes = 0;
-  for (let i = 1; i < n; i++) {
-    if (gcd(n, i) === 1) {
-      totalCoprimes += 1;
+export const hammingWeight = (str: string) => {
+  const strLen = str.length;
+  let result: number = 0;
+  for (let i = 0; i < strLen; i++) {
+    if (str[i] !== '0') {
+      result += 1;
     }
   }
-  return totalCoprimes;
+
+  return result;
 };
 
-export const isPowerOfTwo = (n: number) => n && (n & (n - 1)) === 0;
-
-export const extractExponent = (n: number, base: number) => {
-  for (let i = 1; i < n; i++) {
-    if (Math.pow(n, 1 / i) === base) return i;
+export const strToHex = (str: string) => {
+  var arr = [];
+  for (var i = 0; i < str.length; i++) {
+    arr[i] = ('00' + str.charCodeAt(i).toString(16)).slice(-4);
   }
-  return 0;
-};
-export const carmichaelFunction = (n: number): number => {
-  console.log('calculating carmichaels value for ' + n + '...');
-  if (n === 1) return n;
-  const factorPair = getFactorPair(n) || [n];
-
-  //   console.log(factorPair);
-  const handleNumber = (num: number) => {
-    //  console.log('handleNumber', num);
-    if (isPowerOfOddPrime(num)) {
-      // console.log('isPowerOfOddPrime', num);
-      return eulersTotient(num);
-    }
-    if (isPowerOfTwo(num)) {
-      // console.log('isPowerOfTwo', num);
-      const exponent = extractExponent(num, 2);
-      if (exponent === 0) return 0;
-      if (exponent < 3) {
-        return Math.pow(2, exponent - 1);
-      } else {
-        return Math.pow(2, exponent - 2);
-      }
-    }
-    if (isPrime(num)) {
-      // console.log('isPrime', num);
-      return num - 1;
-    } else {
-      const coprimePair = getCoprimeFactorPair(num);
-      // console.log('isDifferent', num);
-
-      if (coprimePair) {
-        return lcm(
-          carmichaelFunction(coprimePair[0]),
-          carmichaelFunction(coprimePair[1])
-        );
-      }
-
-      return 0;
-    }
-  };
-
-  if (isPrime(n) || isPowerOfOddPrime(n) || isPowerOfTwo(n)) {
-    return handleNumber(n);
-  } else {
-    console.log('AAAAAA');
-    const finalNumbers: number[] = factorPair.map(handleNumber);
-    return lcmm(finalNumbers);
-  }
+  return arr.join(' ');
 };
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,89 @@
-import { carmichaelFunction, genRandomPrime } from './functions.js';
+import {
+  gcd,
+  genRandomPrime,
+  hammingWeight,
+  lcm,
+  modularInverse,
+  randomInt,
+  strToHex,
+} from './functions.js';
 
 //DOM queries
-const privateKeyDisplay = document.querySelector('#private-key-display');
-const publicKeyDisplay = document.querySelector('#public-key-display');
-const inputMessage = document.querySelector('#input-message');
-const inputFriendPublicKey = document.querySelector('#input-friend-public-key');
+const privateKeyDisplay = document.querySelector(
+  '#private-key-display'
+) as HTMLSpanElement;
+const publicKeyDisplayN = document.querySelector(
+  '#public-key-display_n'
+) as HTMLSpanElement;
+const publicKeyDisplayE = document.querySelector(
+  '#public-key-display_e'
+) as HTMLSpanElement;
+
+const inputMessage = document.querySelector(
+  '#input-message'
+) as HTMLTextAreaElement;
+const inputFriendPublicKeyN = document.querySelector(
+  '#input-friend-public-key_n'
+) as HTMLInputElement;
+const inputFriendPublicKeyE = document.querySelector(
+  '#input-friend-public-key_e'
+) as HTMLInputElement;
+
 const outputEncryptedMessage = document.querySelector(
   '#output-encrypted-message'
-);
+) as HTMLTextAreaElement;
 const inputEncryptedMessage = document.querySelector(
   '#input-encrypted-message'
-);
+) as HTMLTextAreaElement;
 const outputDecryptedMessage = document.querySelector(
   '#output-decrypted-message'
-);
-
-//functions
-
-const calculate = (n: number) => {
-  console.log('the value for ' + n, 'is:', carmichaelFunction(n));
-};
-
-calculate(469464541);
+) as HTMLTextAreaElement;
+const encryptBtn = document.querySelector('#encrypt-btn') as HTMLButtonElement;
 //key generation
-const p = genRandomPrime(10);
-const q = genRandomPrime(15);
-
+const p = genRandomPrime(9);
+const q = genRandomPrime(12);
 const n = p * q;
-console.log(p, q);
+const nLambda = lcm(p - 1, q - 1);
 
+const es: { n: number; bitLength: number; hammingWeight: number }[] = [];
+for (let i = 0; i < 10; i++) {
+  let e: number;
+  do {
+    e = randomInt(2, nLambda - 1);
+  } while (gcd(e, nLambda) !== 1);
+  es.push({
+    n: e,
+    bitLength: Math.log2(e + 1),
+    hammingWeight: hammingWeight(e.toString()),
+  });
+}
+
+const prevEs = [...es];
+const sortedEs = es.sort((a, b) => {
+  if (a.bitLength + a.hammingWeight < b.bitLength + b.hammingWeight) return -1;
+  if (a.bitLength + a.hammingWeight > b.bitLength + b.hammingWeight) return 1;
+  return 0;
+});
+
+let e: number = sortedEs[0].n;
+
+const d = modularInverse(e, nLambda);
+if (privateKeyDisplay) {
+  privateKeyDisplay.innerHTML = d.toString();
+}
+
+if (publicKeyDisplayN && publicKeyDisplayE) {
+  publicKeyDisplayN.innerHTML = n.toString();
+  publicKeyDisplayE.innerHTML = e.toString();
+}
+console.log('d is:', d, strToHex('Hello world!'));
+
+//UI handling
+
+// encryptBtn &&
+//   encryptBtn.addEventListener('click', () => {
+//     const message = inputEncryptedMessage.value;
+//     const friendsKey = inputFriendPublicKeyN.value;
+//     const friendsExponent = inputFriendPublicKeyE.value;
+//   });
 //every positive integer n  positive integer λ(n), defined as the smallest positive integer m such that for every integer a between 1 and n that is coprime to n
